bcb4206a839cc144c6b361639131010377b859ae
Bryan Phelps <bryphe@outlook.com>
2018-01-18 12:30:48 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+module.exports = (robot) => {
+  console.log('oni bot started')
+
+  // New user welcoming
+  robot.on('issues.opened', async context => {
+    const response = await context.github.issues.getForRepo(context.repo({
+      state: 'all',
+      creator: context.payload.issue.user.login
+    }))
+
+    const countIssue = response.data.filter(data => !data.pull_request)
+    if (countIssue.length === 1) {
+      try {
+        const config = await context.config('config.yml')
+        if (config.newIssueWelcomeComment) {
+          context.github.issues.createComment(context.issue({body: config.newIssueWelcomeComment}))
+        }
+      } catch (err) {
+        if (err.code !== 404) {
+          throw err
+        }
+      }
+    }
+  })
+}
